0d0c9f28a7dedab09ef825ca3c8c782632f20da1
Rodrigo <rodrigomareque8@gmail.com>
2024-02-06 16:44:40 -0300

    Programando quiz em arquivo JS

--- javascript.js
+++ javascript.js
@@ -91,7 +91,26 @@ const perguntas = [
     },
 ];
 
+const quiz = document.querySelector('#quiz')
+const template = document.querySelector('template')
+
+
 for(const item of perguntas) {
+    const quizItem = template.content.cloneNode(true)
+    quizItem.querySelector('h3').textContent = item.pergunta
     
 
+    for(let resposta of item.respostas) {
+        const dt = quizItem.querySelector('dl dt').cloneNode(true)
+        dt.querySelector('span').textContent = resposta
+
+        quizItem.querySelector('dl').appendChild(dt)
+
+    }
+
+    quizItem.querySelector('dl dt').remove()
+
+    quiz.appendChild(quizItem)
+
 }
+
